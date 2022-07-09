543791490a6e053caa70d98bf0dc39bffdb7b0f3
Anastasiia44 <106679067+Anastasiia44@users.noreply.github.com>
2022-07-10 00:09:35 +0300

    added weather components

---
+++ src/Weather.js
@@ -0,0 +1,44 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-md-9">
+            <input
+              type="search"
+              placeholder="Enter a city..."
+              className="form-control"
+            />
+          </div>
+          <div className="col-md-3">
+            <input type="submit" value="Search" className="btn btn-primary" />
+          </div>
+        </div>
+      </form>
+      <h1>New York</h1>
+      <ul>
+        <li>Saturday 16:00</li>
+        <li>Mostly Cloudy</li>
+      </ul>
+      <div className="row">
+        <div className="col-md-6">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+            alt="Mostly Cloudy"
+          />
+          28°C
+        </div>
+        <div className="col-md-6">
+          <ul>
+            <li>Precipitation: 0%</li>
+            <li>Humidity: 45%</li>
+            <li>Wind: 11 km/h</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
