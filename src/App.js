adefe5dd413161a70ec05f62e73fa6f0b6f5c8c6
Anastasiia44 <106679067+Anastasiia44@users.noreply.github.com>
2022-07-09 23:12:43 +0300

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,21 +4,23 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was created by{" "}
-        <a href="" target="_blank">
-          Anastasiia Maliuha
-        </a>{" "}
-        and is
-        <a
-          href="https://github.com/Anastasiia44/react-weather-app"
-          target="_blank"
-        >
-          {" "}
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was created by{" "}
+          <a href="" target="_blank">
+            Anastasiia Maliuha
+          </a>{" "}
+          and is
+          <a
+            href="https://github.com/Anastasiia44/react-weather-app"
+            target="_blank"
+          >
+            {" "}
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
