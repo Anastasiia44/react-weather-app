af6efffae3c131dfbfaf957fa97b8cf4372972d0
Anastasiia44 <106679067+Anastasiia44@users.noreply.github.com>
2022-07-12 20:18:52 +0300

    added weather forecast and removed Fahrenheit

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,46 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 
 export default function WeatherTemperature(props) {
-  const [unit, setUnit] = useState("celsius");
-
-  function displayFahrenheit(event) {
-    event.preventDefault();
-    setUnit("fahrenheit");
-  }
-
-  function displayCelsius(event) {
-    event.preventDefault();
-    setUnit("celsius");
-  }
-
-  function fahrenheit() {
-    return (props.celsius * 9) / 5 + 32;
-  }
-
-  if (unit === "celsius") {
-    return (
-      <span>
-        <span className="temperature">{Math.round(props.celsius)}</span>
-        <span className="unit">
-          °C |{" "}
-          <a href="/" onClick={displayFahrenheit}>
-            {" "}
-            °F
-          </a>
-        </span>
-      </span>
-    );
-  } else {
-    return (
-      <span>
-        <span className="temperature">{Math.round(fahrenheit())}</span>
-        <span className="unit">
-          <a href="/" onClick={displayCelsius}>
-            °C{" "}
-          </a>
-          | °F
-        </span>
-      </span>
-    );
-  }
+  return (
+    <span className="temperature">
+      {Math.round(props.celsius)}
+      <span className="unit">°C</span>
+    </span>
+  );
 }
