55cf7a98ab05525dad08f944f19ce1c23f4dbaed
Anastasiia44 <106679067+Anastasiia44@users.noreply.github.com>
2024-10-14 10:28:54 +0300

    Update Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -22,7 +22,7 @@ export default function Weather(props) {
   }
 
   function search() {
-    const apiKey = "5fac1c82a1b209a04fbd8df775e5cf4b";
+    const apiKey = "xxxx";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handelResponse);
   }
